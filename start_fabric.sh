494771770f9af99245934a21bd571f2b8fa8b2c7
CrebboElodie <100001938+CrebboElodie@users.noreply.github.com>
2022-09-05 13:21:59 +0100

    Update start_fabric.sh

--- start_fabric.sh
+++ start_fabric.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-exec java -Xms2G -Xmx6G -jar fabric-server-mc.1.19-loader.0.14.9-launcher.0.11.0.jar --nogui
+exec screen -S mcserver java -Xms2G -Xmx6G -jar fabric-server-mc.1.19-loader.0.14.9-launcher.0.11.0.jar --nogui
